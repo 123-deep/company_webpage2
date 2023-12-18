3ac05ba73d2444c61a0bf71cabe1db0f2066f6cd
123-deep <152241662+123-deep@users.noreply.github.com>
2023-12-18 21:46:59 +0530

    Add files via upload

---
+++ js/validation.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+
+	function validation{
+		var user=document.getElementById('name').value;
+
+		if(user=""){
+			document.getElementById('username').innerHTML="please fill the name"
+			return false
+		}
+	}
+</script>
